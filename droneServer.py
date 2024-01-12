f5e962f3d8c17916196445b660d9ed44c038e7f0
Konstantin <kostolom.omega@gmail.com>
2024-01-12 20:18:14 +0400

    first commit

--- droneServer.py
+++ droneServer.py
@@ -1,47 +1,60 @@
-import grpc
+import sys
+from grpc import aio
+import asyncio
+import logging
 import DroneService_pb2
 import DroneService_pb2_grpc
-from concurrent import futures
 from pymavlink import mavutil
-import time
 
 class DroneService(DroneService_pb2_grpc.DroneServiceServicer):
     def __init__(self):
         self.heartbeat = None
         self.detailed_data = None
-    def send_command_long(self, command_type, *args):
-        command_mapping = {
-            "SendArmDisarm": mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-            "SendNavTakeoff": mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-            "SendReturnToLaunch": mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH
-        }
-        method_name = command_type.split("Send")[1]
-
-        command = command_mapping[command_type]
-        master.mav.command_long_send(master.target_system, master.target_component, command, 0, *args)
-
+        self.loop = asyncio.get_running_loop()
+        self.update_task = self.loop.create_task(self.update_data())
+        
+    async def SendArmDisarm(self, request, context):
+        master.mav.command_long_send(master.target_system, master.target_component, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, request.arm, 0, 0, 0, 0, 0, 0)
         msg = master.recv_match(type="COMMAND_ACK", blocking=True)
         return DroneService_pb2.LongAnswer(result=msg.result)
 
-    def SendArmDisarm(self, request, context):
-        return self.send_command_long("SendArmDisarm", request.arm, 0, 0, 0, 0, 0, 0)
+    async def SendNavTakeoff(self, request, context):
+        master.mav.command_long_send(master.target_system, master.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, request.pitch, 0, 0, request.yaw, request.latitude, request.longitude, request.altitude)
+        msg = master.recv_match(type="COMMAND_ACK", blocking=True)
+        return DroneService_pb2.LongAnswer(result=msg.result)
 
-    def SendNavTakeoff(self, request, context):
-        return self.send_command_long("SendNavTakeoff", request.pitch, 0, 0, request.yaw, request.latitude, request.longitude, request.altitude)
-
-    def SendReturnToLaunch(self, request, context):
-        return self.send_command_long("SendReturnToLaunch", 0, 0, 0, 0, 0, 0, 0)
+    async def SendReturnToLaunch(self, request, context):
+        master.mav.command_long_send(master.target_system, master.target_component, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
+        msg = master.recv_match(type="COMMAND_ACK", blocking=True)
+        return DroneService_pb2.LongAnswer(result=msg.result)
     
-    def SendChangeFlightMode(self, request, context):
+    async def SendChangeFlightMode(self, request, context):
         master.mav.set_mode_send(master.target_system, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, request.mode)
         msg = master.recv_match(type="COMMAND_ACK", blocking=True)
         return DroneService_pb2.LongAnswer(result=msg.result)
-    def SendFlightPlan(self, request, context):
-        flightplan = request.flightPlan
-        master.mav.mission_count_send(master.target_system, master.target_component, len(flightplan)+2)
-        master.mav.mission_item_send(master.target_system, master.target_component, 0, 0, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 0, 0, 0) 
         
-        counter = 1     
+    async def SendFlightPlan(self, request, context):
+        flightplan = request.flightPlan
+        master.mav.mission_count_send(master.target_system, master.target_component, len(flightplan)+3)
+        master.mav.mission_item_send(master.target_system, master.target_component, 0, 0, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        firstGeocode = flightplan[0] 
+        
+        master.mav.mission_item_send(master.target_system, master.target_component, 
+                             1,     # Waypoint Number
+                             0, 
+                             mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+                             1,     # Is Current Waypoint
+                             1,     # Should Autocontinue to next wp
+                             0,     # Hold Time (ms)
+                             0,     # Radius for accept within range (meters)
+                             0,     # LOITER Command: Orbit to circle (meters).  Positive clockwise, Negative counter-clockwise
+                             0,     # NAV/LOITER Command: Yaw Orientation [o to 360 degrees]
+                             firstGeocode.latitude,     # local: x position, global: latitude
+                             firstGeocode.longitude,     # local: y position, global: longitude
+                             firstGeocode.altitude)    # local: z position, global: altitude   (meters)
+        
+        counter = 2
+           
         for geocode in flightplan:
             latitude = geocode.latitude
             longitude = geocode.longitude
@@ -67,12 +80,19 @@ class DroneService(DroneService_pb2_grpc.DroneServiceServicer):
                              0, 0, 0, 0, 0, 0, 0, 0, 0)
         
         msg = master.recv_match(type="MISSION_ACK", blocking=True)
-        self.UpdateDetailedData()
+        await self.UpdateDetailedData()
         return DroneService_pb2.LongAnswer(result=msg.type)
-    def GetHeartbeat(self, request, context):
-        self.UpdateHeartbeat()
+    
+    async def update_data(self):
+        while True:
+            await self.UpdateHeartbeat()
+            await self.UpdateDetailedData()
+            await asyncio.sleep(0.5)
+    
+    async def GetHeartbeat(self, request, context):
         return self.heartbeat
-    def UpdateHeartbeat(self):
+            
+    async def UpdateHeartbeat(self):
         msg = master.recv_match(type='HEARTBEAT', blocking=True)
         self.heartbeat = DroneService_pb2.Heartbeat(
             type=msg.type,
@@ -82,10 +102,11 @@ class DroneService(DroneService_pb2_grpc.DroneServiceServicer):
             systemStatus=msg.system_status,
             mavlinkVersion=msg.mavlink_version
         )
-    def GetDetailedData(self, request, context):
-        self.UpdateDetailedData()
+        
+    async def GetDetailedData(self, request, context):
         return self.detailed_data
-    def UpdateDetailedData(self):
+    
+    async def UpdateDetailedData(self):
         systemStatus = master.recv_match(type='SYS_STATUS', blocking=True)
         missionCurrent = master.recv_match(type='MISSION_CURRENT', blocking=True)
         master.waypoint_request_list_send()
@@ -99,60 +120,57 @@ class DroneService(DroneService_pb2_grpc.DroneServiceServicer):
             mission_count = mission_count - 1
 
         self.detailed_data = DroneService_pb2.DetailedData(
-            voltageBattery=systemStatus.voltage_battery,
+            voltageBattery=systemStatus.voltage_battery/1000,
             batteryRemaining=systemStatus.battery_remaining,
-            latitude=position.lat,
-            longitude=position.lon,
-            absoluteAltitude=position.alt,
-            relativeAltitude=position.relative_alt,
-            heading=position.hdg,
+            latitude=position.lat/10000000,
+            longitude=position.lon/10000000,
+            absoluteAltitude=position.alt/1000,
+            relativeAltitude=position.relative_alt/1000,
+            heading=position.hdg/100,
             roll=attitude.roll,
             pitch=attitude.pitch,
             yaw=attitude.yaw,
-            rollSpeed=attitude.rollspeed,
-            pitchSpeed=attitude.pitchspeed,
-            yawSpeed=attitude.yawspeed,
             currentMission=missionCurrent.seq,
             missionCount=mission_count,
-            vx=position.vx,
-            vy=position.vy,
-            vz=position.vz
         )
-    def SendStartMission(self, request, context):
+                
+    async def SendStartMission(self, request, context):
         master.mav.command_long_send(master.target_system, master.target_component,
                              mavutil.mavlink.MAV_CMD_MISSION_START, 0, 1, self.detailed_data.missionCount, 0, 0, 0, 0, 0)
         msg = master.recv_match(type="COMMAND_ACK", blocking=True)
         return DroneService_pb2.LongAnswer(result=msg.result)
 
-    def SendClearAllMissions(self, request, context):
+    async def SendClearAllMissions(self, request, context):
         master.mav.mission_clear_all_send(master.target_system, master.target_component)
         msg = master.recv_match(type="MISSION_ACK", blocking=True)
         return DroneService_pb2.LongAnswer(result=msg.type)
     
-def server_start(port):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+async def server_start(port):
+    server = aio.server()
     DroneService_pb2_grpc.add_DroneServiceServicer_to_server(DroneService(), server)
     server.add_insecure_port('[::]:{0}'.format(port))
-    server.start()
+    await server.start()
     print("Сервер запущен. Порт: {0}.".format(port))
-    try:
-        while True:
-            time.sleep(86400)
-    except KeyboardInterrupt:
-        server.stop(0)
+    await server.wait_for_termination()
 
 SERIAL_PORT = 'COM3'
 BAUDRATE = 57600
 #Альтернативный вариант подключения через интернет:
 host = 'localhost'
-port = 5760
+port = sys.argv[1]
+out_port = int(sys.argv[2])
+#port = 5760
+#out_port = 1604
+
 master = mavutil.mavlink_connection('tcp:{0}:{1}'.format(host, port))
 
-print("Подключаюсь к полётнику...")
+print("Подключение к полётному контроллеру...")
 #master = mavutil.mavlink_connection(SERIAL_PORT, baud=BAUDRATE)
 master.wait_heartbeat()
 
 print("Подключение установлено!")
-master.mav.request_data_stream_send(master.target_system, master.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 1, 1)
-print("Запускаю сервер...") 
-server_start(1604)
+master.mav.request_data_stream_send(master.target_system, master.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 10, 1)
+print("Запускаю сервер...")
+logging.basicConfig(level=logging.INFO)
+
+asyncio.run(server_start(out_port))
